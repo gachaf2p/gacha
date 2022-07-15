1cff846e426cbf926a082f6d15b781d5b218c6bd
gachaf2p <109320776+gachaf2p@users.noreply.github.com>
2022-07-15 12:10:59 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import { MaterialCommunityIcons as Icon} from 'react-native-vector-icons';
+
+export default Class App extends React.Component{render(); {
+    render {
+      <View style ={stylescontainer}>
+      <View style ={{flexDirection:'row'}}>
+      <TouchableOpacity style> = {[styles.tile]}</TouchableOpacity>
+      
+      </View>
+      </View>
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
