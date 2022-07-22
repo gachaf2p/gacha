d1499e43c4dbaf98524402c510bfc9eeebcba635
gachaf2p <109320776+gachaf2p@users.noreply.github.com>
2022-07-22 08:41:49 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 import { MaterialCommunityIcons as Icon} from 'react-native-vector-icons';
 
-export default Class App extends React.Component{render(); {
-    render {
-      <View style ={stylescontainer}>
+export default Class App extends React.Component
+{
+    render() {
+    return;
+        
+      <View style ={styles.container}>
       <View style ={{flexDirection:'row'}}>
+          
       <TouchableOpacity style> = {[styles.tile]}</TouchableOpacity>
+      
       
       </View>
       </View>
